993846a3b335f39e38cd42a824dcfd7872699103
lpsanger <lpsanger@hotmail.com>
2023-02-02 09:51:17 -0300

    refactored first widget, add some backgorund color, some layout adjusts

---
+++ src/lib/components/cardAbout.svelte
@@ -0,0 +1,31 @@
+<style>
+    .image-container {
+        width: 5.75rem;
+        height: 6.0rem;
+        border-radius: 30px;
+        border: solid 1px black;
+        background-size: cover;
+        background-image: URL('/images/foto.jpg');
+    }
+    
+    .content-about {
+        padding: 36px 42px;
+        display: flex;
+        width: 100%;
+        height: 100%;
+        flex-direction: column;
+        align-items: flex-start;
+        -webkit-box-pack: justify;
+        justify-content: space-evenly;
+        box-shadow: transparent 0 0 0 2px inset;
+        position: relative;
+    }
+</style>
+
+<div class="content-about">
+    <div class="image-container" />
+    <p class="my-3 text-base">
+        Eu sou <span class="text-xl font-bold">Leonardo Pereira Sanger</span>, desenvolvedor do
+        Brasil. Tenho interssse em aplicativos, startups, esportes e negócios
+    </p>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
-    import "../app.css";
+    import '../app.css'
   </script>
+  <div class="top-menu">
+      <a href="/">Leonardo Pereira Sanger</a>
+      <div>
+          <a href="/">home</a>
+          <a href="/about">about</a>
+      </div>
+  </div>
   
   <slot />
